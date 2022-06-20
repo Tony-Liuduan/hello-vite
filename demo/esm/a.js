53e049b84e49b45bbe8cf60385c98cb50879287a
liuduan <liuduan@fenbi.com>
2022-06-20 19:23:55 +0800

    ADD: esm demo

---
+++ demo/esm/a.js
@@ -0,0 +1,7 @@
+console.log('a.js start');
+console.log(132);
+
+import { a } from './b.js';
+
+console.log(a);
+console.log('a.js end');
---
+++ demo/esm/b.js
@@ -0,0 +1,5 @@
+console.log('b.js start');
+const a = 1;
+export { a };
+
+console.log('b.js end');
